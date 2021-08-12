89b65c55138d68bb648bdfc300c2c9dc880a2441
Xuân Qúy <81242756+Xuanquy2433@users.noreply.github.com>
2021-08-12 21:21:22 +0700

    React Learn

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,32 @@
 import "./App.css";
-import ColorBox from "./Components/home";
+import ColorBox from "./Components/ColorBox/home";
 import "./ColorBox.scss";
+import { useState } from "react";
+import TodoList from "./Components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "I love Easy Frontend! 😍 " },
+    { id: 2, title: "We love Easy Frontend! 🥰 " },
+    { id: 3, title: "They love Easy Frontend! 🚀 " },
+  ]);
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
-   <div>
-     <ColorBox/>
-   </div>
+    <div>
+      <h1>React Hook Todo-List</h1>
+      <ColorBox />
+
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+    </div>
   );
 }
 
